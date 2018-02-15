3d1cbf8c45aba1d0a0528ed884e77695c8e13ee6
Beth Schofield <bethmschofield@gmail.com>
2018-02-15 22:07:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(x)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  sleep 5
+  seconds = x
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
